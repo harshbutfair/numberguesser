bb5908548895bbf832ee416b0765bf5afa5f48ad
harshbutfair <harsh.narayan@yahoo.com>
2021-06-25 00:13:11 +0530

    added gameOver function

--- app.js
+++ app.js
@@ -28,17 +28,49 @@ guessBtn.addEventListener('click', function(){
 
     // Check if won
     if(guess === winningNum){
-        // Disable input
-        guessInput.disabled = true;
-        // Change border color
-        guessInput.style.borderColor = 'green';
-        // Set message
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+        // Game over Won
+
+        gameOver(true, `${winningNum} is correct, YOU WIN!`)
+
 
     } else {
+        // Wrong number
+        guessesLeft -= 1;
 
+        if(guessesLeft === 0){
+        // Game over, lost
+
+        gameOver(false, `Game over, you lost. The correct number was ${winningNum}`);
+
+        } else {
+        // Game continues - answer wrong
+
+        // Change border color
+        guessInput.style.borderColor = 'red';
+
+        // Clear Input
+        guessInput.value = ''; 
+
+        // Tell user it's the wrong number
+        setMessage(`Guess is not correct, ${guessesLeft} guesses left`, 'red')
+        }
     }
 });
+
+// Game over
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = color;
+    // Change message color
+    message.style.color = color;
+    // Set message
+    setMessage(msg);
+}
 
 // Set Message
 function setMessage(msg, color){
