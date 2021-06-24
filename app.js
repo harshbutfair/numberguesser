d775b6695090cd8d9f8b4ed1db547c6e4e5b4676
harshbutfair <harsh.narayan@yahoo.com>
2021-06-25 01:05:13 +0530

    added random number guesser

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 // UI elements
@@ -81,6 +81,11 @@ function gameOver(won, msg){
     // Play again
     guessBtn.value = 'Play Again';
     guessBtn.className += 'play-again';
+}
+
+// Get Winning Number
+function getRandomNum(min, max){
+    return (Math.floor(Math.random()*(max-min+1)+min));
 }
 
 // Set Message
